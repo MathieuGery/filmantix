79f8b7ee287f99b1fe796fdb10c25426d949a800
mgerylmfr <mathieu.gery@ext.leroymerlin.fr>
2022-09-19 20:32:25 +0200

    added request to model && responce

--- api/endpoints/testModel/testModel.py
+++ api/endpoints/testModel/testModel.py
@@ -2,11 +2,21 @@ from flask_restplus import Resource
 from libs.restplus import api_restplus
 from endpoints.testModel.serializers import model_payload
 from flask import request
+from tmp import get_plot
+# from colorama import Fore, init
+# init(autoreset=True)
 
 import spacy
 
+# print(Fore.GREEN + 'loading model')
 nlp = spacy.load('fr_core_news_lg')
-tokens = nlp('jour nuit matin midi soir miniut')
+# print(Fore.GREEN + 'loaded model')
+
+plot, textLength = get_plot('0133093') #matrix
+
+# print(Fore.GREEN + 'parsing text')
+tokens = nlp(plot)
+# print(Fore.GREEN + 'text parsed')
 
 ns = api_restplus.namespace(
     'testModel', description='Status method')
@@ -17,5 +27,11 @@ class TestModelResources(Resource):
     def post(self):
         word = request.json.get("word")
         compare = nlp(word)
-        print([token.similarity(compare) for token in tokens])
-        return {'message': 'Status Ok'}, 200
+
+        if not compare.has_vector:
+            return {'message': 'no vector'}, 400
+
+        return {'message': 'Status Ok' , 'similarity': [
+                {str(token): token.similarity(compare) for token in tokens}
+            ],
+            'textLength': textLength}, 200,
