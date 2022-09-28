3dfd712d04c622e64bd69b5433a1ec1ca663ca13
MathieuGery <mathieu.gery@epitech.eu>
2022-09-28 09:52:32 +0200

    POC front web

---
+++ web/src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Spencer Sharp - Software designer, founder, and amateur astronaut
+        </title>
+        <meta
+          name="description"
+          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+        />
+      </Head>
+    </>
+  )
+}
