fb3e5265d27b1e668b30f4f7cbc76dc115d70527
MathieuGery <mathieu.gery@epitech.eu>
2022-09-28 11:51:25 +0200

    Add text

---
+++ web/src/components/Plot.jsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+
+export function Plot({ children, className }) {
+  return (
+    <div className={clsx(className, "border-2 border-zinc-300 rounded-xl p-5")}>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+      <h1 className="text-white">Yopu touch my tralalal</h1>
+    </div>
+  )
+}
--- web/src/pages/index.jsx
+++ web/src/pages/index.jsx
@@ -1,4 +1,5 @@
 import { Container } from '@/components/Container'
+import { Plot } from '@/components/Plot'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import Head from 'next/head'
 
@@ -19,7 +20,7 @@ export default function Home() {
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
             Bienvenue sur Filmantix ! 🎬🍿
           </h1>
-          <div className='grid grid-cols-1 sm:grid-cols-2 gap-20 mt-10'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-20 mt-10'>
             <div class="border-2 border-teal-500 rounded-xl px-5 py-5 flex-1 text-center">
               <p className="text-l font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
                 Les carrés <span className="text-teal-500">100</span>
@@ -53,6 +54,9 @@ export default function Home() {
             required
             className="min-w-0 placeholder:p-1 flex-auto appearance-none rounded-md border ring-zinc-800 dark:ring-white border-zinc-900/10 bg-white ring-2 px-2 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-l"
           />
+        </div>
+        <div>
+          <Plot className="mt-10"/>
         </div>
       </Container>
     </>
