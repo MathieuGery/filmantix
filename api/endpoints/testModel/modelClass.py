932e79178f007e22edd7f135c1e88ce04ac24694
JulienGery <julien.gery@yahoo.com>
2022-09-30 17:06:23 +0200

    correction classModel

--- api/endpoints/testModel/modelClass.py
+++ api/endpoints/testModel/modelClass.py
@@ -17,7 +17,7 @@ class Model:
         return  self._tokens, self.words
     
     def update(self, pPlot = None):
-        plot = pPplot if pPlot else self.dbPlot
+        plot = pPlot if pPlot else self.dbPlot
         self.day = plot.get('to_char')
         self._tokens = self.nlp(''.join(items['word'] for items in plot.get('plot_non_obsucred')))
         self.deleteCopy()
