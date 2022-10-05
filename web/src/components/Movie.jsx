0030191a447498b8c98a3d13e778b77a8193a6ab
MathieuGery <mathieu.gery@epitech.eu>
2022-10-05 22:38:34 +0200

    Add link to the movie

--- web/src/components/Movie.jsx
+++ web/src/components/Movie.jsx
@@ -1,7 +1,9 @@
 import clsx from 'clsx'
 import Image from 'next/image';
-import { Button } from './Button';
+import Link from 'next/link';
+
 function Movie({ data, className }) {
+  console.log(data)
   return (
     <div>
       <div className={clsx(className, "border border-teal-500 rounded-xl p-5 mt-4")}>
@@ -18,7 +20,7 @@ function Movie({ data, className }) {
                 className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
               />
             </div>
-            <div className="flex-1 space-y-2">
+            <div className="flex-1">
               <p className="text-2xl font-bold tracking-tight text-white mt-4 mb-5 sm:ml-10">
                 {data.title}
               </p>
@@ -26,7 +28,7 @@ function Movie({ data, className }) {
                 Pays d'origine: <span className="text-teal-500">{data.origin_country}</span>
               </p>
               <p className="text-l font-bold tracking-tight text-white sm:ml-14">
-                Directeur: <span className="text-teal-500">{data.director}</span>
+                Réalisateur: <span className="text-teal-500">{data.director}</span>
               </p>
               <p className="text-l font-bold tracking-tight text-white sm:ml-14">
                 Durée: <span className="text-teal-500">{data.runtime}</span><span className="text-xs">m</span>
@@ -34,11 +36,11 @@ function Movie({ data, className }) {
               <p className="text-l font-bold tracking-tight text-white sm:ml-14">
                 Sortie: <span className="text-teal-500">{data.release_date}</span>
               </p>
-              <p className="text-l font-bold tracking-tight text-white sm:ml-14">
-                bouton
-              </p>
             </div>
           </div>
+              <Link href={data.link} className="text-center text-l font-bold hover:bg-teal-700 bg-teal-500 rounded-xl shadow-xl py-2 text-white mt-2">
+                {data.title}
+              </Link>
         </div>
       </div >
     </div>
